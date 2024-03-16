1b96b59892ff220c13cf0d1bfd71daf841252020
André Marcello <160755361+eodede@users.noreply.github.com>
2024-03-16 20:09:19 -0300

    Add files via upload

---
+++ Perimetro.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Perimetro {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número, (1) (2) (3)");
+        int numero = scanner.nextInt();
+
+    }
+}
